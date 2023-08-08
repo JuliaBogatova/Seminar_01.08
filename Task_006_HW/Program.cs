82d0137553ac139614bbd5f0c30194ae7e8f11a0
Юлия Богатова <z.yuliya25_02@icloud.com>
2023-08-08 18:59:55 +0300

    Task 6 completed

--- Task_006_HW/Program.cs
+++ Task_006_HW/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая:
+// 1. на вход принимает число
+// 2. и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} - четное число.");
+}
+else
+{
+    Console.WriteLine($"{num} - не является четным числом.");
+}
