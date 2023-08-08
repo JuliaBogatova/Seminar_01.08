17b6abb07ef0c3242bf1061c96f64d1d8094e2ea
Юлия Богатова <z.yuliya25_02@icloud.com>
2023-08-08 17:52:53 +0300

    Task 5 completed

--- Task_005/Program.cs
+++ Task_005/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая:
+// 1. на вход принимает одно число (N),
+// 2. а на выходе показывает все целые числа в промежутке от -N до N
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+Console.Write("Введите целое положительное число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+while(count <= num)
+{
+    Console.Write($"{count} ");
+    count++;
+}
