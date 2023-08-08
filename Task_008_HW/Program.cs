59b1fafbf199f41cadfd5b0ed64a9d9313c26996
Юлия Богатова <z.yuliya25_02@icloud.com>
2023-08-08 19:29:09 +0300

    Task 8 completed

--- Task_008_HW/Program.cs
+++ Task_008_HW/Program.cs
@@ -4,3 +4,14 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Write("Введите целое положительное число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num)
+{
+    if (count % 2 == 0)
+        Console.Write($"{count} ");
+    count++;
+}
