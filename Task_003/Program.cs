ca117744d1bfc3b260f6c7afb2c815e865985c7b
Юлия Богатова <z.yuliya25_02@icloud.com>
2023-08-08 17:40:40 +0300

    Added changes in task 3

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -6,37 +6,37 @@
 // 5 -> Пятница
 
 Console.Write("Введите номер дня недели от 1 до 7: ");
-int day = Convert.ToInt32(Console.ReadLine());
+string day = Console.ReadLine();
 
-if (day == 1)
+if (day == "1")
 {
     Console.WriteLine("Понедельник");
 }
-else if(day == 2)
+else if(day == "2")
 {
     Console.WriteLine("Вторник");
 }
-else if(day == 3)
+else if(day == "3")
 {
     Console.WriteLine("Среда");
 }
-else if(day == 4)
+else if(day == "4")
 {
     Console.WriteLine("Четверг");
 }
-else if(day == 5)
+else if(day == "5")
 {
     Console.WriteLine("Пятница");
 }
-else if(day == 6)
+else if(day == "6")
 {
     Console.WriteLine("Суббота");
 }
-else if(day == 7)
+else if(day == "7")
 {
     Console.WriteLine("Воскресенье");
 }
 else
 {
-    Console.WriteLine("Ошибка! Дня недели с таким номером не существует. Введите номер от 1 до 7.");
+    Console.WriteLine("Ошибка! Некорректный ввод. Введите номер от 1 до 7.");
 }
