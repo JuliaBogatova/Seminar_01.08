669e492deb6b052d7c83bd3d8065801b3373fb74
Юлия Богатова <z.yuliya25_02@icloud.com>
2023-08-08 17:11:31 +0300

    Task 1 completed

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -7,3 +7,15 @@
 // a = 9, b = -3 -> да
 // a = -3, b = 9 -> нет
 
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberOne / numberTwo == numberTwo)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
